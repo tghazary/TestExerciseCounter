6d51f309df253562aeb73af6da639683d704d61f
Tigran Ghazaryan <tigran.ghazaryan@Tigrans-MacBook-Pro.local>
2023-07-18 14:19:24 +0400

    Add error handling for not allowed numbers

--- src/main/java/com/example/demo/service/CounterService.java
+++ src/main/java/com/example/demo/service/CounterService.java
@@ -3,6 +3,8 @@ package com.example.demo.service;
 import com.example.demo.model.Counter;
 import com.example.demo.repositories.CounterRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class CounterService {
@@ -12,17 +14,20 @@ public class CounterService {
         this.counterRepository = counterRepository;
     }
 
+    @Transactional(readOnly = true, isolation = Isolation.READ_COMMITTED)
     public int getCounterValue() {
         Counter counter = counterRepository.findById(1L).orElse(new Counter());
         return counter.getValue();
     }
 
+    @Transactional(isolation = Isolation.READ_COMMITTED)
     public void incrementCounter() {
         Counter counter = counterRepository.findById(1L).orElse(new Counter());
         counter.setValue(counter.getValue() + 1);
         counterRepository.save(counter);
     }
 
+    @Transactional(isolation = Isolation.READ_COMMITTED)
     public void setCounterValue(int value) {
         Counter counter = counterRepository.findById(1L).orElseThrow(() -> new IllegalStateException("Counter not found"));
         counter.setValue(value);
--- src/main/java/com/example/demo/ui/CounterUI.java
+++ src/main/java/com/example/demo/ui/CounterUI.java
@@ -6,12 +6,19 @@ import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Pattern;
 import org.springframework.beans.factory.annotation.Autowired;
 
 @Route("")
 public class CounterUI extends VerticalLayout {
     private final CounterService counterService;
 
+
+    @Max(value = Integer.MAX_VALUE, message = "Value should not exceed 100")
+    @Min(value = Integer.MIN_VALUE, message = "Value should be greater than or equal to 0")
+    @Pattern(regexp = "\\d+", message = "Invalid value. Only digits are allowed.")
     private final TextField counterTextField;
 
     public CounterUI(@Autowired CounterService counterService) {
@@ -21,6 +28,7 @@ public class CounterUI extends VerticalLayout {
 
         Button incrementButton = new Button("Increment");
         incrementButton.addClickListener(e -> {
+            validateNumber(counterTextField);
             counterService.incrementCounter();
             updateCounterValue();
             Notification.show("Counter value incremented");
@@ -28,6 +36,7 @@ public class CounterUI extends VerticalLayout {
 
         Button saveButton = new Button("Save");
         saveButton.addClickListener(e -> {
+            validateNumber(counterTextField);
             int newValue = Integer.parseInt(counterTextField.getValue());
             counterService.setCounterValue(newValue);
             updateCounterValue();
@@ -42,5 +51,20 @@ public class CounterUI extends VerticalLayout {
     private void updateCounterValue() {
         int counterValue = counterService.getCounterValue();
         counterTextField.setValue(String.valueOf(counterValue));
+    }
+
+    private void validateNumber(TextField counterTextField) {
+        String value = counterTextField.getValue();
+        if (counterTextField.isInvalid() || value == null || value.isEmpty() || !value.matches("\\d+")) {
+            counterTextField.setErrorMessage("Invalid value. Only integer values are allowed.");
+            counterTextField.setInvalid(true);
+        } else {
+            try {
+                Integer.parseInt(value);
+            } catch (NumberFormatException e) {
+                counterTextField.setErrorMessage("Invalid value. Only integer values are allowed.");
+                counterTextField.setInvalid(true);
+            }
+        }
     }
 }
